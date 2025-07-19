e12095c7867c5fdd2b6bac66e6d8b14a2e750185
Khudo #R <mirkhud97@gmail.com>
2025-07-19 14:37:42 +0900

    update unocss config

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,9 @@ import { TasknestButton } from '@/components/ui/Button'
 </script>
 
 <template>
-  <TasknestButton label="Click Me" />
+  <TasknestButton label="Click Me" variant="destructive" />
+  <TasknestButton label="Click Me" variant="primary" />
+  <TasknestButton label="Click Me" variant="secondary" />
+  <TasknestButton label="Click Me" variant="outline" />
+  <TasknestButton label="Click Me" variant="link" />
 </template>
